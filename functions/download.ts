a40eaa7ddd93c30bef25b095b591c4daf93f50d3
Donghyuk Jang <bluesy.panbie@gmail.com>
2026-04-26 20:16:46 +0900

    [Feat] "go.akify.io/download" 엔드포인트 신규 추가 (#7)
    * feat: 매물 공유 링크 og 태그 동적 주입 (#5)
    
    * feat: 다운로드 데스크톱 QR 페이지 추가
    
    * feat: 다운로드 엔드포인트 추가
    
    * chore: 다운로드 trailing slash 정규화

---
+++ functions/download.ts
@@ -0,0 +1,36 @@
+interface Env {
+  ASSETS: Fetcher;
+}
+
+const IOS_STORE =
+  'https://apps.apple.com/kr/app/%EC%95%84%ED%82%A4%ED%8C%8C%EC%9D%B4/id6751454780';
+const ANDROID_STORE =
+  'https://play.google.com/store/apps/details?id=com.jammering.akify';
+
+export const onRequestGet: PagesFunction<Env> = async (context) => {
+  const { request, env } = context;
+  const ua = request.headers.get('user-agent') || '';
+  const url = new URL(request.url);
+  const utm = url.searchParams.toString();
+
+  if (/iPhone|iPad|iPod/.test(ua)) {
+    const target = utm ? `${IOS_STORE}?${utm}` : IOS_STORE;
+    return Response.redirect(target, 302);
+  }
+
+  if (/Android/.test(ua)) {
+    const referrer = utm ? `&referrer=${encodeURIComponent(utm)}` : '';
+    return Response.redirect(`${ANDROID_STORE}${referrer}`, 302);
+  }
+
+  const pageUrl = new URL('/download/index.html', request.url);
+  const pageResponse = await env.ASSETS.fetch(new Request(pageUrl.toString()));
+
+  return new Response(pageResponse.body, {
+    status: 200,
+    headers: {
+      'content-type': 'text/html; charset=utf-8',
+      'cache-control': 'public, s-maxage=300',
+    },
+  });
+};
